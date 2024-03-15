339c3e73141e2d5778be5e9369da44ac4d4654dc
Richard Rodger <richard@ricebridge.com>
2024-03-15 23:28:51 +0000

    pdm-be-v0.5.23

--- backend/dist/handler/lambda/ingest.js
+++ backend/dist/handler/lambda/ingest.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const lambda_1 = require("../../env/lambda/lambda");
 function complete(seneca) {
+    seneca.listen({ type: 'sqs', pin: 'aim:ingest,process:episode' });
     seneca.client({ type: 'sqs', pin: 'aim:store,download:audio' });
+    seneca.client({ type: 'sqs', pin: 'aim:ingest,process:episode' });
 }
 exports.handler = async (event, context) => {
     let seneca = await (0, lambda_1.getSeneca)('ingest', complete);
--- backend/src/handler/lambda/ingest.ts
+++ backend/src/handler/lambda/ingest.ts
@@ -1,7 +1,9 @@
 import { getSeneca } from '../../env/lambda/lambda'
 
 function complete(seneca: any) {
+  seneca.listen({type:'sqs',pin:'aim:ingest,process:episode'})
   seneca.client({type:'sqs',pin:'aim:store,download:audio'})
+  seneca.client({type:'sqs',pin:'aim:ingest,process:episode'})
 }
 
 exports.handler = async (
